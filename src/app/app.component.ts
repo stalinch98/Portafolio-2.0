9136ca15d317cec96cf25087c7b52f8908bb3aee
Stalin Chiguano <stalin_dany98@hotmail.com>
2024-04-02 23:58:15 -0500

    Add Tailwind CSS configuration and footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
